e25449385071d94fb9d8cdf4a29b64006a7939cb
annett21 <annettarasevich@gmail.com>
2023-05-22 17:47:47 +0300

    added select

--- sqlmodel-tutorial/models.py
+++ sqlmodel-tutorial/models.py
@@ -1,4 +1,4 @@
-from sqlmodel import Field, Session, SQLModel, create_engine
+from sqlmodel import Field, Session, SQLModel, create_engine, select, or_
 
 
 class Hero(SQLModel, table=True):
@@ -27,6 +27,10 @@ def create_heroes():
     hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
     hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
     hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
+    hero_4 = Hero(name="Tarantula", secret_name="Natalia Roman-on", age=32)
+    hero_5 = Hero(name="Black Lion", secret_name="Trevor Challa", age=35)
+    hero_6 = Hero(name="Dr. Weird", secret_name="Steve Weird", age=36)
+    hero_7 = Hero(name="Captain North America", secret_name="Esteban Rogelios", age=93)
 
     session = Session(engine)
 
@@ -34,13 +38,26 @@ def create_heroes():
         session.add(hero_1)
         session.add(hero_2)
         session.add(hero_3)
+        session.add(hero_4)
+        session.add(hero_5)
+        session.add(hero_6)
+        session.add(hero_7)
 
         session.commit()
 
 
+def select_heroes():
+    with Session(engine) as session:
+        statement = select(Hero).where(or_(Hero.age <= 35, Hero.age > 90))
+        results = session.exec(statement)
+        for hero in results:
+            print(hero)
+
+
 def main():
-    create_db_and_tables()
-    create_heroes()
+    # create_db_and_tables()
+    # create_heroes()
+    select_heroes()
 
 
 if __name__ == "__main__":
